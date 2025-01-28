e9b69c0c26084d9412ddeda638cd6ad87f2484db
Half-blood-prince-2710 <guptadmanish2710@gmail.com>
2025-01-28 11:54:44 +0530

    create middleware to count hits

--- main.go
+++ main.go
@@ -1,24 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"sync/atomic"
 )
 
 type apiConfig struct {
-	fileserverHots atomic.Int32
+	fileserverHits atomic.Int32
 }
 
 func main() {
+	apiCfg := apiConfig{}
 	 mux := http.NewServeMux()
-	mux.Handle("/app/",http.StripPrefix("/app",http.FileServer(http.Dir("./"))))
+	mux.Handle("/app/",apiCfg.middlewareMetricsInc(http.StripPrefix("/app",http.FileServer(http.Dir("./")))))
 	mux.HandleFunc("/healthz",func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type","text/plain; charset=utf-8")
 		w.WriteHeader(200)
 		w.Write([]byte("OK"))
 	})
-
+	mux.HandleFunc("/metrics",func(w http.ResponseWriter, r *http.Request) {
+		fmt.Sprintf("Hits: ",apiCfg.fileserverHits)
+	})
 	 srv:= &http.Server{
 		Handler: mux,
 		Addr: ":8080",
