918e6aa6a9341e38092e63cef988e0fcbb6c78d2
Half-blood-prince-2710 <guptadmanish2710@gmail.com>
2025-02-03 18:24:31 +0530

    chirp scema created

---
+++ sql/schema/002_chirps.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+CREATE TABLE IF NOT EXISTS users(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    body TEXT NOT NULL ,
+    user_id UUID,
+    FOREIGN KEY (user_id)
+    REFERENCES users(id)
+    ON DELETE CASCADE
+);
+
+
+-- +goose Down
+DROP TABLE IF EXISTS chirps;
