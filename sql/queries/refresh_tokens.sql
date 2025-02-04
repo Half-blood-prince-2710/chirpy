8d9fe8ad23530be038b667602c006cf87da9675e
Half-blood-prince-2710 <guptadmanish2710@gmail.com>
2025-02-04 20:41:57 +0530

    query written

--- sql/queries/refresh_tokens.sql
+++ sql/queries/refresh_tokens.sql
@@ -4,3 +4,8 @@ VALUES ($1,$2,$3) RETURNING *;
 
 -- name: GetTokenByToken :one
 SELECT * FROM refresh_tokens WHERE token = $1;
+
+-- name: UpdateRefreshToken :exec 
+UPDATE refresh_tokens
+SET expires_at = $1 , updated_at = $2 
+WHERE token = $3;
