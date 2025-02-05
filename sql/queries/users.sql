5ec9f19695526d9beeca09d91be898f2145a41b7
Half-blood-prince-2710 <guptadmanish2710@gmail.com>
2025-02-05 11:48:19 +0530

    query updated

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -10,5 +10,5 @@ SELECT id, created_at,updated_at,email,hashed_password FROM users WHERE email = 
 
 -- name: UpdateUser :one
 UPDATE users 
-SET email = $1 , hashed_password = $2
-where id = $3;
+SET email = $1 , hashed_password = $2, updated_at = $3
+where id = $4 RETURNING id, created_at,updated_at,email;
